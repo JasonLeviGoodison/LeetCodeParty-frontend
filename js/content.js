221c1909c3bb1c7c201f5e4785781c19d22da117
Jason Goodison <jasongoodison1234@gmail.com>
2020-07-08 17:02:28 -0700

    Gets the inital socket connection and userId set up

--- js/content.js
+++ js/content.js
@@ -1,18 +1,25 @@
-//
-const ENDPOINT = "http://127.0.0.1:4001";
-
-
-
-function injectJsScript(script) {
-    var script = document.createElement('script');
-    script.src = script;
-    (document.head || document.documentElement).appendChild(script);
-    script.onload = function () {
-        script.parentNode.removeChild(script);
-    };
-}
 
 window.addEventListener ("load", myMain, false);
+
+const ENDPOINT = "http://127.0.0.1:4001";
+console.log("about to make a connection")
+var socket = io(ENDPOINT);
+
+var curRoom = {
+    userId: "",
+    roomId: "",
+    problemId: "",
+    socket: ""
+}
+
+console.log("Socket,", socket)
+
+socket.on('userId', function(data) {
+    if (curRoom.userId === "") {
+        console.log(data)
+        curRoom.userId = data;
+    }
+});
 
 function myMain (evt) {
     let submitButton = $("[data-cy=\"run-code-btn\"]");
@@ -21,12 +28,92 @@ function myMain (evt) {
         let lang = $(".ant-select-selection-selected-value").attr("title");
         console.log("Using lang", lang);
     });
-
 }
 
-function destroyPreview() {
-    // max-width: inherit;
-    // width: 100%;
-}
+// interaction with the popup
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.type === 'getInitData') {
+        sendResponse({
+          roomId: curRoom.roomId,
+          //chatVisible: getChatVisible()
+        });
+        return;
+      }
 
+      if (request.type === 'createRoom') {
+        socket.emit('createRoom', {
+          problemId: request.data.problemId
+        }, function(data) {
+        //   initChat();
+        //   setChatVisible(true);
+        //   lastKnownTime = data.lastKnownTime;
+        //   lastKnownTimeUpdatedAt = new Date(data.lastKnownTimeUpdatedAt);
+        //   messages = [];
+        //   sessionId = data.sessionId;
+        //   ownerId = request.data.controlLock ? userId : null;
+        //   state = data.state;
+        //   videoId = request.data.videoId;
+        //   pushTask(broadcast(false));
+          sendResponse({
+            roomId: curRoom.roomId
+          });
+        });
+        return true;
+      }
 
+      if (request.type === 'joinSession') {
+        socket.emit('joinSession', request.data.sessionId, function(data) {
+          if (data.errorMessage) {
+            sendResponse({
+              errorMessage: data.errorMessage
+            });
+            return;
+          }
+
+          if (data.videoId !== request.data.videoId) {
+            socket.emit('leaveSession', null, function(data) {
+              sendResponse({
+                errorMessage: 'That session is for a different video.'
+              });
+            });
+            return;
+          }
+
+        //   initChat();
+        //   setChatVisible(true);
+        //   sessionId = request.data.sessionId;
+        //   lastKnownTime = data.lastKnownTime;
+        //   lastKnownTimeUpdatedAt = new Date(data.lastKnownTimeUpdatedAt);
+        //   messages = [];
+        //   for (var i = 0; i < data.messages.length; i += 1) {
+        //     addMessage(data.messages[i]);
+        //   }
+        //   ownerId = data.ownerId;
+        //   state = data.state;
+        //   videoId = request.data.videoId;
+        //   pushTask(receive(data));
+          sendResponse({});
+        });
+        return true;
+      }
+
+      if (request.type === 'leaveSession') {
+        socket.emit('leaveSession', null, function(_) {
+          sessionId = null;
+          setChatVisible(false);
+          sendResponse({});
+        });
+        return true;
+      }
+
+      if (request.type === 'showChat') {
+        if (request.data.visible) {
+          setChatVisible(true);
+        } else {
+          setChatVisible(false);
+        }
+        sendResponse({});
+      }
+    }
+  );
