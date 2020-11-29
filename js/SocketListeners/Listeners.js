32aa624874f71f2a8ff05765134cef8da27ebd2c
Jason Goodison <jasongoodison1234@gmail.com>
2020-11-28 19:26:32 -0800

    updating on room started

--- js/SocketListeners/Listeners.js
+++ js/SocketListeners/Listeners.js
@@ -81,6 +81,7 @@ function handleUserSubmitted(curRoom, userId, meta) {
 
 function handleRoomStarted(curRoom) {
     curRoom.startRoom();
+    sideBar.enqueue("hey youtube, the room started", "info");
     SendMessageToPopup(UPDATE_DOM_MESSAGE, curRoom, function(response) {});
 }
 
